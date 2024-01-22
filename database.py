c1e33605cf8073fe6d8349469e1cbadc038eb168
Dias Sagatov <101350324+diassagatov@users.noreply.github.com>
2024-01-22 10:29:59 +0600

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE = 'mssql+pyodbc://diassagatov:megD34RT@34.107.97.52:1433/data?driver=ODBC+Driver+17+for+SQL+Server'
+URL_DATABASE = 'sqlite:///./data.db'
 
 engine = create_engine(URL_DATABASE)
 
